ed60b2c388f69335535ffcacfc24effc8495b3ca
kymEwhain <ymdwang200@ewhain.net>
2024-12-18 16:49:25 +0900

    Update import.py

--- drawing/import.py
+++ drawing/import.py
@@ -1,6 +1,9 @@
 import cv2
-import mediapipe as mp
-import numpy as np
-from fastdtw import fastdtw
-from scipy.spatial.distance import euclidean
+import threading
 import time
+import subprocess
+import numpy as np
+import mediapipe as mp
+from scipy.spatial.distance import euclidean
+from fastdtw import fastdtw
+from dtaidistance import dtw
