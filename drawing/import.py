23f4565d310a2137e8f383ad0bc6b66183e5bcf9
kymEwhain <ymdwang200@ewhain.net>
2024-12-19 12:00:53 +0900

    Delete drawing directory

--- drawing/import.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import threading
-import time
-import subprocess
-import numpy as np
-import mediapipe as mp
-from scipy.spatial.distance import euclidean
-from fastdtw import fastdtw
-from dtaidistance import dtw
